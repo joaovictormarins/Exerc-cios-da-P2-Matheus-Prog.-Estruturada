12639cb8374f56c4dc8b2c711231073bce45b25f
joaovictormarins <166523898+joaovictormarins@users.noreply.github.com>
2024-06-25 08:35:36 -0300

    Add files via upload

---
+++ # 6 Encontrar Maior e Menor Número Em Um.py
@@ -0,0 +1,10 @@
+# 6 Encontrar Maior e Menor Número Em Uma Lista 
+
+numero = []
+
+numero = (input("Digite a lista de numeros em ordem pondo espaço : ")) .split() 
+num_max = max(numero) 
+num_min = min(numero)
+
+print(f"O maior número na lista é: {num_max}")
+print(f"O menor número na lista é: {num_min} /n")
---
+++ #6.py
@@ -0,0 +1,12 @@
+#6. Encontrar o Maior e o Menor Número em uma Lista:
+
+numero = []
+
+numero = input("Digite a lista de numero separados por espaço : ") .split()
+num_max = max(numero)
+num_min = min(numero)
+
+print(f"O maior numero da lista é: {num_max}")
+print(f"O menor numero da lista é: {num_min} \n")
+
+print (numero)
