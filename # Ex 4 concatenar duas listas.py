3b4defc8ecb8340cef791d98f6a92805592eb26f
joaovictormarins <166523898+joaovictormarins@users.noreply.github.com>
2024-07-09 08:07:55 -0300

    Add files via upload

---
+++ # Ex 4 concatenar duas listas.py
@@ -0,0 +1,10 @@
+# ex 4 concatenar duas listas 
+lista_1 = []
+lista_2 = []
+
+lista_1 = input("Adicine os itens na lista 1 colocando espaço entre cada item : ") .split()
+lista_2 = input("Adicine os itens na  lista 2 colocando espaço entre cada item : ") .split()
+
+lista_3 = lista_1 + lista_2
+
+print (lista_3)
